ba8fd11d8f40d215f6f97bd0401d3d91462919d0
M Saad Traique <maliksaad109109@gmail.com>
2024-06-10 01:37:00 -0700

    Update TableCreate
    -Set Order {Asec , Desc}
    -Set Limit {5,10,15,20}

--- src/Components/TableCreate.jsx
+++ src/Components/TableCreate.jsx
@@ -1,35 +1,10 @@
 import { useState, useEffect } from "react";
-
-
-const data = [
-    {
-      "id": 1,
-      "title": "Mens Casual Premium Slim Fit T-Shirts ",
-      "price": 22.3,
-      "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-      "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-    },
-    {
-      "id": 2,
-      "title": "Mens Cotton Jacket",
-      "price": 55.99,
-      "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-      "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-    },
-    {
-      "id": 3,
-      "title": "Mens Casual Slim Fit",
-      "price": 15.99,
-      "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-      "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-    },
-  ]
-
-
-
+   
 
 function TableCreate(){
-    const[products,setProducts]=useState([])
+    const[products,setProducts]=useState([]);
+    const[orderBy,setOrderBy]=useState("asc")
+    const[limitBy,setLimitBy]=useState(20)
     const[image,setImage]=useState("");
     const[title,setTitle]=useState("");
     const[price,setPrice]=useState("");
@@ -55,13 +30,13 @@ function TableCreate(){
         setProducts(newData)
     }
 
+
+
     useEffect(() => {
-        fetch('https://fakestoreapi.com/products?limit=5').then((res) => {
-          return res.json();
-        }).then((data) => {
-          setProducts(data)
-        })
-      }, []);
+        fetch(`https://fakestoreapi.com/products?sort=${orderBy}&limit=${limitBy}`)
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+      }, [orderBy,limitBy]);
 
     return(
         <>
@@ -87,7 +62,21 @@ function TableCreate(){
                     </div>
                 </div>
 
-                <div className="row">
+                <div className="row justify-content-center">
+
+                    <div className="mt-3 d-flex justify-content-around">
+                        <div>
+                            <button className={`btn btn-sm me-3 ${(orderBy == 'asc')? "btn-warning" : "btn-outline-warning" }`} onClick={()=> setOrderBy("ase")}>ASC</button>
+                            <button className={`btn btn-sm ${(orderBy == 'desc')? "btn-warning" : "btn-outline-warning" }`} onClick={()=> setOrderBy("desc")}>DESC</button>
+                        </div>
+                        <div>
+                            <button className={`btn btn-sm me-1  ${(limitBy == 5)? "btn-success" : "btn-outline-success" }`} onClick={()=> setLimitBy(5)}>5</button>
+                            <button className={`btn btn-sm me-1  ${(limitBy == 10)? "btn-success" : "btn-outline-success" }`} onClick={()=> setLimitBy(10)}>10</button>
+                            <button className={`btn btn-sm me-1  ${(limitBy == 15)? "btn-success" : "btn-outline-success" }`} onClick={()=> setLimitBy(15)}>15</button>
+                            <button className={`btn btn-sm me-1  ${(limitBy == 20)? "btn-success" : "btn-outline-success" }`} onClick={()=> setLimitBy(20)}>20</button>
+                        </div>
+                        
+                    </div>
                     <table className="table table-warning table-striped mt-3 ">
                         <thead>
 
